302ca3af70c9d4d58962f80855c72b584dd81e19
Joshua Gregory <yoshig5114@gmail.com>
2023-06-23 18:53:46 -0400

    completed

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ import express from "express";
 import userRouter from "./users.route.js";
 
 const router = express.Router();
-
+router.use("/users", userRouter);
 router.get("/test", (req, res) => {
   res.send("working");
 });
 
 // TODO: use the imported router to handle all routes matching "/users"
-router.use("/users", userRouter);
+
 export default router;
